3fc1f2d0f7a8bc30d6e5fb64122e858a7d45def0
杨喆军 <yangzhejun@13GRAY.local>
2022-07-23 17:02:31 +0800

    feat:PodcastModule

--- chat_module/__init__.py
+++ chat_module/__init__.py
@@ -1,10 +1,12 @@
 from . import chatter
 from . import guess_idiom
 from . import douban
+from . import podcast
 
 Modules = [
     guess_idiom.GuessIdiomModule(),
     douban.DoubanModule(),
+    podcast.PodcastModule(),
 
     ## 默认聊天
     chatter.ChatterModule(),
